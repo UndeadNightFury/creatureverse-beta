69e7176094b7ede0b07fff61870e2ae5450cebad
Undead Night Fury <undeadnightfury@gmail.com>
2026-01-22 19:58:09 -0500

    countdown script bug fix

--- js/countdown.js
+++ js/countdown.js
@@ -26,12 +26,12 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "v0.4 Release!";
+    document.getElementById("countdown").innerHTML = "v0.5 Release!";
   }
 
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown-mobile").innerHTML = "v0.4 Release!";
+    document.getElementById("countdown-mobile").innerHTML = "v0.5 Release!";
   }
 }, 1000);

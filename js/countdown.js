3eb9558573174be82f33303e9cb46622bd3a9b5a
Undead Night Fury <undeadnightfury@gmail.com>
2026-01-22 19:53:53 -0500

    updating the countdown script

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Sep 20, 2024").getTime();
+var countDownDate = new Date("March 15, 2026").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
